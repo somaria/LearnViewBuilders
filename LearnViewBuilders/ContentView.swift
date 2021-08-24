fb67e03e5c45fd49f0e7ee647a9d634f53627194
Daryl <daryl.9@gmail.com>
2021-08-24 10:38:02 +0800

    Initial Commit

---
+++ LearnViewBuilders/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnViewBuilders
+//
+//  Created by admin on 24/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnViewBuilders/LearnViewBuildersApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnViewBuildersApp.swift
+//  LearnViewBuilders
+//
+//  Created by admin on 24/8/21.
+//
+
+import SwiftUI
+
+@main
+struct LearnViewBuildersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
